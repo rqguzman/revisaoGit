aa967769f982dcb3980b236b2da89e4822ff5721
rqguzman <rafael.guzman@outlook.com>
2019-07-12 15:26:33 -0300

    nova linha

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,11 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello, Git!");
 
+		System.out.println("WhattaGit!");
+		
 	}
 
 }
